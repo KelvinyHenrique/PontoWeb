7d822d33cfc1a2ac78d0acf4b821ae24222e9ec1
Kelviny <kelvinyhenrique17@gmail.com>
2019-07-03 11:49:04 -0300

    Atualizando alguns dados

--- assets/php/index.php
+++ assets/php/index.php
@@ -3,9 +3,8 @@
 $valorbtn='';
 if(isset($_POST['btnvalue'])){
 $valorbtn = $_POST['btnvalue'];
-header("Location: ../html/$valorbtn.php");
+header("Location: ../html/$valorbtn.html");
 } else {
-    header("Location: ../html/admin.html");
+    header("Location: ../html/index.html");
 }
-
 ?>
